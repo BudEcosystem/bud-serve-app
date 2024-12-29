3566cf7561154079918a7181617e6cc1f8211f91
Varun S R <varun@bud.studio>
2024-12-29 23:37:49 +0530

    feat: added cluster status update workflow

--- budapp/cluster_ops/services.py
+++ budapp/cluster_ops/services.py
@@ -20,7 +20,7 @@ import json
 import tempfile
 from datetime import datetime, timezone
 from typing import Any, Dict, List, Tuple
-from uuid import UUID
+from uuid import UUID, uuid4
 
 import aiohttp
 import yaml
@@ -509,6 +509,9 @@ class ClusterService(SessionMixin):
             )
             await BudNotifyService().send_notification(notification_request)
 
+            # Create request to trigger cluster status update periodic task
+            await self._perform_cluster_status_update_request(db_cluster.cluster_id, db_workflow.created_by)
+
     async def delete_cluster_from_notification_event(self, payload: NotificationPayload) -> None:
         """Delete a cluster in database.
 
@@ -849,4 +852,44 @@ class ClusterService(SessionMixin):
             logger.exception(f"Failed to send cancel cluster onboarding request: {e}")
             raise ClientException(
                 "Failed to cancel cluster onboarding", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            ) from e
+
+    async def _perform_cluster_status_update_request(self, cluster_id: UUID, current_user_id: UUID) -> Dict:
+        """Perform update cluster node status request to bud_cluster app.
+
+        Args:
+            cluster_id: The ID of the cluster to update.
+            current_user_id: The ID of the current user.
+        """
+        update_cluster_endpoint = f"{app_settings.dapr_base_url}v1.0/invoke/{app_settings.bud_cluster_app_id}/method/cluster/update-node-status"
+
+        try:
+            payload = {
+                "cluster_id": str(cluster_id),
+                "notification_metadata": {
+                    "name": BUD_INTERNAL_WORKFLOW,
+                    "subscriber_ids": str(current_user_id),
+                    "workflow_id": str(uuid4()),
+                },
+                "source_topic": f"{app_settings.source_topic}",
+            }
+            logger.debug(
+                f"Performing update cluster node status request. payload: {payload}, endpoint: {update_cluster_endpoint}"
+            )
+            async with aiohttp.ClientSession() as session, session.post(
+                update_cluster_endpoint, json=payload
+            ) as response:
+                response_data = await response.json()
+                if response.status != 200:
+                    logger.error(f"Failed to update cluster node status: {response.status} {response_data}")
+                    raise ClientException(
+                        "Failed to update cluster node status", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+                    )
+
+                logger.debug("Successfully updated cluster node status")
+                return response_data
+        except Exception as e:
+            logger.exception(f"Failed to send update cluster node status request: {e}")
+            raise ClientException(
+                "Failed to update cluster node status", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
             ) from e
