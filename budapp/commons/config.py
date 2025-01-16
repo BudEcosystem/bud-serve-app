68e2a1ff3703ed513bf648e41a81f122879b0dca
Varun S R <varun@bud.studio>
2025-01-16 20:43:33 +0530

    fix: added periodic sync in secret

--- budapp/commons/config.py
+++ budapp/commons/config.py
@@ -357,8 +357,10 @@ class SecretsConfig(BaseConfig):
     dapr_api_token: Optional[str] = Field(None, alias="DAPR_API_TOKEN")
     password_salt: str = Field("bud_password_salt", alias="PASSWORD_SALT")
     jwt_secret_key: str = Field(alias="JWT_SECRET_KEY")
-    redis_password: str = Field(alias="SECRETS_REDIS_PASSWORD")
-    redis_uri: str = Field(alias="SECRETS_REDIS_URI")
+    redis_password: str = Field(
+        alias="REDIS_PASSWORD", json_schema_extra=enable_periodic_sync_from_store(is_global=True)
+    )
+    redis_uri: str = Field(alias="REDIS_URI", json_schema_extra=enable_periodic_sync_from_store(is_global=True))
 
     @computed_field
     def redis_url(self) -> str:
