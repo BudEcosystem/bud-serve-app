7c6aae1352e681ec87b26ea2e78425c3b3a126d7
Varun S R <varun@bud.studio>
2025-01-17 12:43:31 +0530

    feat: added cluster resources in cluster update

--- budapp/cluster_ops/services.py
+++ budapp/cluster_ops/services.py
@@ -612,10 +612,15 @@ class ClusterService(SessionMixin):
         )
         logger.debug(f"Cluster retrieved successfully: {db_cluster.id}")
 
+        # Update data
+        update_data = {"status": payload.content.result["status"]}
+
+        # Get cluster resources from event
+        cluster_resources = await self._calculate_cluster_resources(payload.content.result["node_info"])
+        update_data.update(cluster_resources.model_dump(exclude_unset=True, exclude_none=True))
+
         # Update cluster status
-        db_cluster = await ClusterDataManager(self.session).update_by_fields(
-            db_cluster, {"status": payload.content.result["status"]}
-        )
+        db_cluster = await ClusterDataManager(self.session).update_by_fields(db_cluster, update_data)
         logger.debug(f"Cluster {db_cluster.id} status updated to {payload.content.result['status']}")
 
     async def _calculate_cluster_resources(self, data: Dict[str, Any]) -> ClusterResourcesInfo:
