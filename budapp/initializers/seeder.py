6dde52ec432c9c66dc920789e267504a8d21fd0f
Rahul V Ramesh <rahulvramesh@hotmail.com>
2025-04-01 22:48:38 +0700

    refactor: update seeder configuration to include Keycloak and clean up user seeder
    - Added BaseKeycloakSeeder to the seeders configuration.
    - Commented out UserSeeder for potential future use.
    - Ensured DatasetSeeder is correctly referenced without duplication.

--- budapp/initializers/seeder.py
+++ budapp/initializers/seeder.py
@@ -1,18 +1,21 @@
+from budapp.initializers.base_keycloak_seeder import BaseKeycloakSeeder
 from budapp.initializers.cloud_model_seeder import CloudModelSeeder
 from budapp.initializers.cloud_provider_seeder import CloudProviderSeeder
+from budapp.initializers.dataset_seeder import DatasetSeeder
 from budapp.initializers.icon_seeder import IconSeeder
 from budapp.initializers.provider_seeder import ProviderSeeder
-from budapp.initializers.template_seeder import TemplateSeeder
-from budapp.initializers.user_seeder import UserSeeder
 from budapp.initializers.quantization_method_seeder import QuantizationMethodSeeder
-from budapp.initializers.dataset_seeder import DatasetSeeder
+from budapp.initializers.template_seeder import TemplateSeeder
+
+
 seeders = {
-    "user": UserSeeder,
+    "keycloak": BaseKeycloakSeeder,
+    # "user": UserSeeder,
     "provider": ProviderSeeder,
     "cloud_model": CloudModelSeeder,
     "icon": IconSeeder,
     "template": TemplateSeeder,
     "cloud_provider": CloudProviderSeeder,
     "quantization_method": QuantizationMethodSeeder,
-    "datasets": DatasetSeeder,
+    "datasets": DatasetSeeder
 }
